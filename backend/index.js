f2e283fc65918f5d8e5ecab3ffb0881f4a285cd4
Alex Szymański <2alex0607@gazeta.pl>
2025-07-29 12:18:15 +0200

    please_review_this_shit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
+
+//ROUTER FOR ALCOHOL
+const alcoholRouter = express.Router();
+app.use('/alcohol', alcoholRouter);
+
+alcoholRouter.get('/random', (req, res) => {
+    res.send("RANDOM");
+});
+
+alcoholRouter.get('/daily', (req, res) => {
+    res.send("DAILY");
+});
+
+//ROUTER FOR ACCOUNT
+const accountRouter = express.Router();
+app.use('/account', accountRouter);
+
+accountRouter.post('/login', (req, res) => {
+    res.send("LOGIN");
+});
+
+accountRouter.post('/logout', (req, res) => {
+    res.send("LOGOUT");
+});
+
+accountRouter.post('/register', (req, res) => {
+    res.send("REGISTER");
+});
