1642e2997c2127f31b239f7e7a29a54c6e157494
Dzarooo <kacperjaroszewski06@gmail.com>
2025-07-29 10:19:49 +0200

    created empty project and installe all needed packages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const axios = require('axios');
+  
+  return (
+    <>
+      <div>Hello world! This project uses react as frontend, tailwind for style and express for backend. Axios on frontend is supposed to make requests to backend There is nodemon installed on backend so when you start backend and change something, it will update <span className="text-blue-500">automatically</span>.</div>
+    </>
+  )
+}
+
+export default App
