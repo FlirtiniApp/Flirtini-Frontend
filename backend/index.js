150dd7a657bbbdcd3eaf3997f890d43a529bc46d
Wiktor Ilczuk <137764780+Wilczuk1506@users.noreply.github.com>
2025-07-29 15:52:46 +0200

    connecting to database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+//Connect to databes before connecting to backend
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@flirtini.5tabe6e.mongodb.net/Flirtini?retryWrites=true&w=majority&appName=Flirtini`)
+.then(() => {
+    console.log("connected to db");
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
 })
+.catch((err) => {
+    console.log("something done goofed");
+    console.log(err);
+});
