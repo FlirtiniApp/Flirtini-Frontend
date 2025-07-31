4a9e349b7156eff8e8772ad2efab079e00740ff5
awitkowsky <witkowski.thebill@gmail.com>
2025-07-31 09:20:50 +0200

    SIDEBAR MA DOBRĄ POZYCJĘ. DODANE PAGE NOT FOUND

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,17 +19,19 @@ function Layout() {
     "/liked",
     "/compose",
     "/todays-choice",
+<<<<<<< Updated upstream:frontend/src/App.jsx
     // jak jakaś ścieżka ma nie mieć sidebara to wystarczy jej tu NIE WPISAĆ
   ].includes(path);
+=======
+    "/",
+    "/profile",
+  ].some((prefix) => path.startsWith(prefix));
+>>>>>>> Stashed changes:src/App.jsx
 
   return (
     <div className="flex h-screen bg-gray-900 text-white">
       {showSidebar && <Sidebar />}
-      <main
-        className={`flex-1 overflow-y-auto p-8 ${
-          showSidebar ? "ml-[11vw]" : ""
-        }`}
-      >
+      <main className="flex-1 overflow-y-auto p-8">
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Explore />} />
@@ -38,6 +40,7 @@ function Layout() {
             <Route path="/compose" element={<Compose />} />
             <Route path="/todays-choice" element={<TodaysChoice />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<div>Page Not Found</div>} />
           </Routes>
         </Suspense>
       </main>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,9 +2,19 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
+<<<<<<< Updated upstream:frontend/src/components/Sidebar.jsx
     <div className="h-[96vh] fixed w-[11vw] flex flex-col justify-between my-[2vh] px-4 border-r-2 border-gray-500">
       <div className="flex items-center gap-2">
         <img className="w-[3vw]" src="/Flirtini/logo.png" alt="Flirtini Logo" />
+=======
+    <div className="h-[96vh] relative w-[11vw] min-w-fit flex flex-col justify-between my-[2vh] px-4 border-r-2 border-gray-500">
+      <div className="flex items-center gap-2">
+        <img
+          className="w-[50px]"
+          src="/Flirtini/logo.png"
+          alt="Flirtini Logo"
+        />
+>>>>>>> Stashed changes:src/components/Sidebar.jsx
         <h1 className="text-white text-2xl font-bold font-[Dancing_Script]">
           Flirtini
         </h1>
